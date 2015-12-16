d87db411bb6c081f94d71e487deace451a634681
Aivar Annamaa <aivar.annamaa@gmail.com>
2015-12-16 17:49:30 +0200

    build script unification

--- tkinterhtml/tkhtml/build_tkhtml_unix.sh
+++ tkinterhtml/tkhtml/build_tkhtml_unix.sh
@@ -5,8 +5,6 @@ git clone https://github.com/olebole/tkhtml3.git
 
 cd tkhtml3
 
-TCL=/Library/Frameworks/Tcl.framework/Versions/8.6/
-
 # Generate some source files (see readme in https://github.com/starseeker/tcltk/tree/master/tkhtml)
 tclsh src/cssprop.tcl
 tclsh src/tokenlist.txt
@@ -21,13 +19,7 @@ cd build
 
 # configure, make and install
 chmod 755 ../configure
-../configure #\
-	#--prefix=$PREFIX \
-	#--with-tcl=$PREFIX/lib \
-	#--with-tk=$PREFIX/lib \
-	#--with-tclinclude=$PREFIX/include \
-	#--with-tkinclude=$PREFIX/include
-
+../configure
 make
 
 
